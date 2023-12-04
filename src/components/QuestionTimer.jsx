19b4ce87881eb4b4d71cc09660dda44b69c2dca7
Elvan Hidayat <elvanhd@gmail.com>
2023-12-05 06:07:27 +0700

    using effect cleanup function

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -5,14 +5,22 @@ export default function QuestionTimer({ timeout, onTimeout }) {
 
   useEffect(() => {
     console.log('SETTING TIMEOUT');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
     console.log('SETTING INTERVAL');
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id='question-time' max={timeout} value={remainingTime} />;
