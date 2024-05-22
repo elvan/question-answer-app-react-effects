9a418089aca9b496b997f524c54ac08e7cf531e5
Elvan Hidayat <elvanhd@gmail.com>
2024-05-23 00:50:46 +0700

    Add quiz functionality with questions and answers
    - Import and use Quiz component in App.jsx
    - Create Quiz.jsx with state setup for questions and answers
    - Add handleSelectAnswer function to capture user answers
    - Create questions.js with sample questions and answers data
    - Display questions and answers dynamically based on state

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,30 @@
 import { useState } from 'react';
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
